c42d408176481e9c0ddec13dfb3ed4e47d3af43c
Andrea <montanari.andrea98@gmail.com>
2022-08-19 18:27:00 +0200

    Client: added model for drawing from deck

--- Briscola_multiplayer/Client.py
+++ Briscola_multiplayer/Client.py
@@ -16,7 +16,6 @@ pygame.display.set_caption("Bouncing Ball")
 #     ctypes.windll.user32.ShowWindow(HWND, SW_MAXIMIZE)
 
 screen_w, screen_h = screen.get_size()
-print(screen_w, " ", screen_h)
 background = pygame.image.load("table6.png")
 background = pygame. transform. scale(background, (screen_w, screen_h))
 
@@ -28,19 +27,32 @@ class Card:
         self.rect = self.face.get_rect()
         self.rect.center = position
         self.turned = False
-        print(self.position)
+        self.vel = 10
+
+        self.hand_position = position
+
+    def set_hand_position(self, hand_position):
+        self.hand_position = hand_position
+
+    def move_from_deck_to_hand(self):
+        """
+        Handles the card drawing from the deck
+        """
+        if self.position[0] > self.hand_position[0]:
+            self.position[0] -= self.vel
+        if self.position[1] < self.hand_position[1]:
+            self.position[1] += self.vel
+        self.rect.center = self.position
 
     def draw(self, screen):
         if not self.turned:
             screen.blit(self.face, self.rect)
         else:
             screen.blit(self.back, self.rect)
+        self.move_from_deck_to_hand()
 
     def turn(self):
         self.turned = not self.turned
-
-    def move(self):
-        self.position[0] += 1
 
     # TODO: stato della carta (in mazzo, in mano, in quale mano, ...)
 
@@ -103,11 +115,17 @@ class Button:
 
 # Buttons creation
 btn_exit = Button('X', (screen_w - 100, 50), 40, 40, border_raduis=30, over_color='#D74B4B')
-btn_start = Button('START', (80, 50), 100, 40, border_raduis=30)
+btn_draw = Button('DRAW', (80, 50), 100, 40, border_raduis=30)
 
 # Cards creation
-asso_denari = Card([screen_w//2, screen_h//2], "1D")
-
+deck = {}
+card_suits = ['B', 'C', 'D', 'S']
+for i in range(0, 40):
+    deck_key = str(i%10) + card_suits[i//10]
+    deck[deck_key] = Card([screen_w-200, screen_h//2], "back")
+dummy_deck = pygame.image.load("svg-napoletane/back.png")
+dummy_deck_rect = dummy_deck.get_rect()
+dummy_deck_rect.center = [screen_w - 200, screen_h // 2]
 
 running = True
 clock = pygame.time.Clock()
@@ -117,22 +135,25 @@ while running:
             running = False
             pygame.quit()
 
-    # pygame.time.delay(500)
-
     screen.blit(background, (0, 0))
+    pygame.draw.circle(screen, (255,0,0), (screen_w//2, screen_h//2), 4) # TODO: remove
 
     # Buttons
     btn_exit.draw(screen)
-    button_pressed = btn_exit.check_click()
-    if button_pressed:
+    if btn_exit.check_click():
         running = False
         pygame.quit()
-    btn_start.draw(screen)
+    btn_draw.draw(screen)
 
-    # Draw card on screen
-    asso_denari.draw(screen)
-    # asso_denari.turn()
-    asso_denari.move()
+    # Draw cards on screen
+    screen.blit(dummy_deck, dummy_deck_rect)    # Draws a dummy deck
+    deck.get("1D").draw(screen) # TODO: questo rappresenterebbe il mazzo. Va fatto meglio (es: non avendo degli oggeti Card ma solo 2/3 immagini sovrapposte)
+    deck.get("2D").draw(screen)
+    deck.get("3D").draw(screen)
+    if btn_draw.check_click():
+        deck.get("1D").set_hand_position([screen_w//2+120, screen_h-200])
+        deck.get("2D").set_hand_position([screen_w//2, screen_h-200])
+        deck.get("3D").set_hand_position([screen_w//2-120, screen_h-200])
 
     pygame.display.update()
     clock.tick(60)
